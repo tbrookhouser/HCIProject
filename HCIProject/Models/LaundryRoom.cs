b040d977aec8ffe17caec403066b416fc4e84333
Tara Brookhouser <tarabrookhouser@gmail.com>
2018-03-06 12:48:19 -0600

    Added Models
    Added data models for machines and rooms

---
+++ HCIProject/Models/LaundryRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HCIProject.Models
+{
+    public class LaundryRoom
+    {
+        public List<Washer> washerList;
+        public List<Dryer> dryerList;
+    }
+}
